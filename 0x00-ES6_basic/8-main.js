ac8b35bd86e95951b72c54baf4fc2b9d8482ef57
Onah-NeLson <Onahneslon2@gmail.com>
2023-07-25 05:11:59 +0000

    ES6_basics js

---
+++ 0x00-ES6_basic/8-main.js
@@ -0,0 +1,4 @@
+/* The intent of this script is to, eslint-disable import/extensions */
+import getBudgetForCurrentYear from './8-getBudgetCurrentYear.js';
+
+console.log(getBudgetForCurrentYear(2100, 5200, 1090));

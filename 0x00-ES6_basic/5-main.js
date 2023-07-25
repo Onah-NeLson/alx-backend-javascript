c0fe5bc24eeefd6d0246008bda32f9032a7fd487
Onah-NeLson <Onahneslon2@gmail.com>
2023-07-25 05:01:15 +0000

    ES6_basics js

---
+++ 0x00-ES6_basic/5-main.js
@@ -0,0 +1,4 @@
+/* My Hands-on, intent of this script is to ,eslint-disable import/extensions */
+import concatArrays from './5-spread-operator.js';
+
+console.log(concatArrays(['a', 'b'], ['c', 'd'], 'Hello'));

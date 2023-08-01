fcdc82aa0b4d72fc6b5ce43f8edfd56735ae39ce
Onah-NeLson <Onahneslon2@gmail.com>
2023-08-01 23:41:21 +0000

    ES6_data_manipulation

---
+++ 0x03-ES6_data_manipulation/5-main.js
@@ -0,0 +1,5 @@
+// The next line of code is a special comment that disables ESLint's 'import/extensions' rule for the entire file.
+/* eslint-disable import/extensions */
+import createInt8TypedArray from './5-typed_arrays.js';
+
+console.log(createInt8TypedArray(10, 2, 89));
---
+++ 0x03-ES6_data_manipulation/6-main.js
@@ -0,0 +1,5 @@
+// The next line of code is a special comment that disables ESLint's 'import/extensions' rule for the entire file.
+/* eslint-disable import/extensions */
+import setFromArray from './6-set.js';
+
+console.log(setFromArray([12, 32, 15, 78, 98, 15]));

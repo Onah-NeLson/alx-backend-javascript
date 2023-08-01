ffe21a8ddd1182a58062140b98fec452103cf8e3
Onah-NeLson <Onahneslon2@gmail.com>
2023-08-01 23:04:12 +0000

    ES6_data_manipulation

---
+++ 0x03-ES6_data_manipulation/0-main.js
@@ -0,0 +1,5 @@
+// The next line of code is a special comment that disables ESLint's 'import/extensions' rule for the entire file.
+/* eslint-disable import/extensions */
+import getListStudents from './0-get_list_students.js';
+
+console.log(getListStudents());

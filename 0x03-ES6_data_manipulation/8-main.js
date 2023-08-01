f07ad866bb907f7191ca90054d89976f89307dcc
Onah-NeLson <Onahneslon2@gmail.com>
2023-08-01 23:49:22 +0000

    ES6_data_manipulation

---
+++ 0x03-ES6_data_manipulation/8-main.js
@@ -0,0 +1,6 @@
+// The next line of code is a special comment that disables ESLint's 'import/extensions' rule for the entire file.
+/* eslint-disable import/extensions */
+import cleanSet from './8-clean_set.js';
+
+console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), 'bon'));
+// console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), ''));
---
+++ 0x03-ES6_data_manipulation/9-main.js
@@ -0,0 +1,5 @@
+// The next line of code is a special comment that disables ESLint's 'import/extensions' rule for the entire file.
+/* eslint-disable import/extensions */
+import groceriesList from './9-groceries_list.js';
+
+console.log(groceriesList());
